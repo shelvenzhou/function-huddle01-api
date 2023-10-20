b811b2c2efae243ad670da0bd0b43d8e82520e16
Shelven Zhou <favoring.zhou49@gmail.com>
2023-10-20 03:22:44 +0000

    update e2e test

--- test/TestOracleConsumerContract.ts
+++ test/TestOracleConsumerContract.ts
@@ -6,7 +6,8 @@ import { execSync } from "child_process";
 async function waitForResponse(consumer: Contract, event: Event) {
   const [, data] = event.args!;
   // Run Phat Function
-  const result = execSync(`phat-fn run --json dist/index.js -a ${data} https://api-mumbai.lens.dev/`).toString();
+  const result = execSync(`phat-fn run --json dist/index.js -a ${data} {\\"apiKey\\":\\"test\\"}`).toString();
+  // console.log(`Oracle result: ${result}`);
   const json = JSON.parse(result);
   const action = ethers.utils.hexlify(ethers.utils.concat([
     new Uint8Array([0]),
@@ -35,20 +36,23 @@ describe("OracleConsumerContract.sol", function () {
     const consumer = await TestOracleConsumerContract.deploy(deployer.address);
 
     // Make a request
-    const profileId = "0x01";
-    const tx = await consumer.request(profileId);
+    const meetingId = "03dec8c8-ad63-4f3a-9615-0a2b0049790d";
+    const tx = await consumer.requestMeetingAirdrop(meetingId);
     const receipt = await tx.wait();
     const reqEvents = receipt.events;
     expect(reqEvents![0]).to.have.property("event", "MessageQueued");
 
     // Wait for Phat Contract response
     const respEvents = await waitForResponse(consumer, reqEvents![0])
+    // console.log(`${JSON.stringify(respEvents)}`);
 
     // Check response data
-    expect(respEvents[0]).to.have.property("event", "ResponseReceived");
-    const [reqId, pair, value] = respEvents[0].args;
+    expect(respEvents[0]).to.have.property("event", "Transfer");
+    expect(respEvents[1]).to.have.property("event", "Transfer");
+    expect(respEvents[2]).to.have.property("event", "ResponseReceived");
+    const [reqId, pair, value] = respEvents[2].args;
     expect(ethers.BigNumber.isBigNumber(reqId)).to.be.true;
-    expect(pair).to.equal(profileId);
+    expect(pair).to.equal(meetingId);
     expect(ethers.BigNumber.isBigNumber(value)).to.be.true;
   });
 });
